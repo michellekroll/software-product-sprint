fb91ec3d2c0030ed9140e30911ebc784a421a7b8
Michelle <michellekroll@berkeley.edu>
2021-03-10 23:34:16 +0000

    fix unused comment

--- portfolio/src/main/java/com/google/sps/servlets/TranslationServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/TranslationServlet.java
@@ -12,9 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/translate")
 public class TranslationServlet extends HttpServlet {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = 1L;
 
     @Override
